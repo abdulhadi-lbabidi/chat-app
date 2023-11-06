5ed394d378a4a90bbcebbeecc9b011e69da0f3f1
unknown <abdalhadilbabidi.codm@gmail.com>
2023-11-06 20:14:23 +0300

    UI and some styling and make the messages orderd by created at

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { useEffect, useState } from "react";
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy } from 'firebase/firestore'
 import { auth, db } from "../firebase-config";
+import '../styles/chat.css'
 
 export const Chat = (props) => {
     const { room } = props;
     const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const messagesRef = collection(db, "messages");
+
+
+    useEffect(() => {
+        const queryMessages = query(messagesRef, where("room", "==", room), orderBy("createdAt"))
+
+        const unsubscibe = onSnapshot(queryMessages, (snapshot) => {
+            let messages = [];
+            snapshot.forEach((doc) => {
+                messages.push({ ...doc.data(), id: doc.id });
+            });
+            setMessages(messages);
+        });
+        return () => unsubscibe();
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,6 +38,17 @@ export const Chat = (props) => {
         setNewMessage("")
     }
     return <div className="chat-app">
+        <div className="header">
+            <h1>welcome to: {room} </h1>
+        </div>
+        <div className="messages" >
+            {messages.map((message) => (
+                <div className="message" key={message.id}>
+                    <span className="user">{message.user}</span>
+                    {message.text}
+                </div>
+            ))}
+        </div>
         <form onSubmit={handleSubmit} className="new-message-form">
             <input
                 className="new-message-input"
