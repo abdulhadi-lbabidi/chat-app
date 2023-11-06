dca3f4dd4f6be0f8b2a71ed8c21bc6f0e047fb9e
unknown <abdalhadilbabidi.codm@gmail.com>
2023-11-06 18:21:04 +0300

    connect to firestore for enter room & send messages

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       {room ? (
-        <Chat />
+        <Chat room={room} />
       ) : (
         <div className='room'>
           <label>Enter Room Name:</label>
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,35 @@
-export const Chat = () => {
-    return <div className="chat-app"> Chat</div>
+import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { auth, db } from "../firebase-config";
+
+export const Chat = (props) => {
+    const { room } = props;
+    const [newMessage, setNewMessage] = useState("");
+
+    const messagesRef = collection(db, "messages");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newMessage === "") return;
+
+        await addDoc(messagesRef, {
+            text: newMessage,
+            createdAt: serverTimestamp(),
+            user: auth.currentUser.displayName,
+            room: room,
+        });
+
+        setNewMessage("")
+    }
+    return <div className="chat-app">
+        <form onSubmit={handleSubmit} className="new-message-form">
+            <input
+                className="new-message-input"
+                placeholder="type your message here"
+                onChange={(e) => setNewMessage(e.target.value)}
+                value={newMessage}
+            />
+            <button className="send-button" type="submit">send </button>
+        </form>
+    </div>;
 }
